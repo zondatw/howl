9235820676cca7c25d45f71e0d04427761de4557
zonda yang <u226699@gmail.com>
2023-06-13 22:22:26 +0800

    Execute command and send ctrl + c singal after 5 sec

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,60 @@ mod cli;
 
 use crate::cli::Args;
 
-fn main() {
+use std::process::exit;
+use std::time::Duration;
+use tokio::process::Command;
+use tokio::signal::unix::{signal, SignalKind};
+
+async fn send_ctrl_c(child: &mut tokio::process::Child) {
+    let timeout_duration = Duration::from_secs(5);
+
+    let ctrl_c_signal =
+        signal(SignalKind::interrupt()).expect("Failed to register Ctrl+C handler.");
+    tokio::pin!(ctrl_c_signal);
+
+    tokio::select! {
+        _ = tokio::time::sleep(timeout_duration) => {
+            // Timeout reached
+            println!("Timeout reached. Sending SIGTERM to the child process.");
+            child.kill().await.expect("Failed to send SIGTERM to the child process.");
+        }
+        _ = ctrl_c_signal.recv() => {
+            // Ctrl+C signal received
+            println!("Ctrl+C signal received. Sending SIGINT to the child process.");
+            child.kill().await.expect("Failed to send SIGINT to the child process.");
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
     let args = Args::new();
 
     println!("Execute command: {:?}", args.execute);
+
+    // Example command and arguments
+    let split_execute: Vec<&str> = args.execute.split(' ').collect();
+    let exec_command = split_execute[0];
+    let exec_args = &split_execute[1..];
+
+    // Spawn sub-process
+    let mut child = Command::new(exec_command)
+        .args(exec_args)
+        .spawn()
+        .expect("Failed to spawn child process.");
+
+    // Send Ctrl+C signal
+    send_ctrl_c(&mut child).await;
+
+    // Wait for the child process to finish
+    let status = child
+        .wait()
+        .await
+        .expect("Failed to wait for child process.");
+
+    if !status.success() {
+        println!("Child process exited with an error.");
+        exit(1);
+    }
 }
